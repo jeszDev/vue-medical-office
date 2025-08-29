5e7b1009b65c11fd28c04d141e594f7418413e4d
jess-github <jjcr.fie@gmail.com>
2025-08-28 22:52:11 -0600

    change: componente input date adecuado a mas tipos

--- src/modules/common/components/InputBaseDate.vue
+++ src/modules/common/components/InputBaseDate.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    type="datetime-local"
+    :type="type"
     class="h-10 w-full rounded-md border bg-background px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground placeholder:text-foreground/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground/5 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
     :name="name"
     :value="modelValue"
@@ -15,12 +15,16 @@
 import { useAttrs } from 'vue';
 
 interface Props {
+  type?: 'date' | 'datetime-local';
   modelValue?: string;
   error?: string;
   name: string;
 }
 
-defineProps<Props>();
+withDefaults(defineProps<Props>(), {
+  type: 'date',
+});
+
 const attrs = useAttrs();
 defineEmits(['update:modelValue', 'blur']);
 </script>

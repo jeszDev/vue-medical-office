616bba5da21db468e46cc0732aaad0e86fcec443
jeszDev <jjcr.dev@gmail.com>
2025-08-29 13:23:57 -0600

    Prop para deterinar si el componete para crear usuario va a ir en un modal

--- src/modules/patient/views/PatientCreate.ts
+++ src/modules/patient/views/PatientCreate.ts
@@ -1,16 +1,16 @@
-import ButtonPrimary from '@/modules/common/components/ButtonPrimary.vue';
-import InputLabel from '@/modules/common/components/InputLabel.vue';
-import InputBase from '@/modules/common/components/InputBase.vue';
-import InputBaseDate from '@/modules/common/components/InputBaseDate.vue';
-import TextArea from '@/modules/common/components/TextArea.vue';
-import { computed, defineComponent, ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
-import { useMutation, useQuery } from '@tanstack/vue-query';
-import { useForm, useFieldArray } from 'vee-validate';
-import * as yup from 'yup';
+import ButtonPrimary from '@/modules/common/components/ButtonPrimary.vue'
+import InputLabel from '@/modules/common/components/InputLabel.vue'
+import InputBase from '@/modules/common/components/InputBase.vue'
+import InputBaseDate from '@/modules/common/components/InputBaseDate.vue'
+import TextArea from '@/modules/common/components/TextArea.vue'
+import { computed, defineComponent, ref, watchEffect } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMutation, useQuery } from '@tanstack/vue-query'
+import { useForm, useFieldArray } from 'vee-validate'
+import * as yup from 'yup'
 
-import { createUpdatePatientAction, getPatientByIdAction } from '../actions';
-import { watch } from 'vue';
+import { createUpdatePatientAction, getPatientByIdAction } from '../actions'
+import { watch } from 'vue'
 
 const schema = yup.object({
   nombre: yup.string().required('Este campo es obligatorio'),
@@ -23,7 +23,7 @@ const schema = yup.object({
   telefono: yup.string(),
   correo_electronico: yup.string(),
   observaciones: yup.string(),
-});
+})
 
 export default defineComponent({
   components: {
@@ -38,9 +38,14 @@ export default defineComponent({
       type: String,
       required: true,
     },
+    isInsideModal: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
-    const router = useRouter();
+    const router = useRouter()
+    // const modal = props.isInsideModal
 
     const {
       data: patient,
@@ -50,7 +55,7 @@ export default defineComponent({
       queryKey: ['patient', props.patientId],
       queryFn: () => getPatientByIdAction(props.patientId),
       retry: false,
-    });
+    })
 
     const {
       mutate,
@@ -59,58 +64,60 @@ export default defineComponent({
       data: updatedPatient,
     } = useMutation({
       mutationFn: createUpdatePatientAction,
-    });
+    })
 
     const { values, defineField, validateField, errors, handleSubmit, resetForm, meta } = useForm({
       validationSchema: schema,
-    });
-    const [nombre, nombreAttrs] = defineField('nombre');
-    const [primer_apellido, primer_apellidoAttrs] = defineField('primer_apellido');
-    const [segundo_apellido, segundo_apellidoAttrs] = defineField('segundo_apellido');
-    const [fecha_nacimiento, fecha_nacimientoAttrs] = defineField('fecha_nacimiento');
-    const [telefono, telefonoAttrs] = defineField('telefono');
-    const [correo_electronico, correo_electronicoAttrs] = defineField('correo_electronico');
-    const [observaciones, observacionesAttrs] = defineField('observaciones');
+    })
+    const [nombre, nombreAttrs] = defineField('nombre')
+    const [primer_apellido, primer_apellidoAttrs] = defineField('primer_apellido')
+    const [segundo_apellido, segundo_apellidoAttrs] = defineField('segundo_apellido')
+    const [fecha_nacimiento, fecha_nacimientoAttrs] = defineField('fecha_nacimiento')
+    const [telefono, telefonoAttrs] = defineField('telefono')
+    const [correo_electronico, correo_electronicoAttrs] = defineField('correo_electronico')
+    const [observaciones, observacionesAttrs] = defineField('observaciones')
 
     const onSubmit = handleSubmit(async (values) => {
-      console.log('onsubmit');
-      console.log({ values });
+      console.log('onsubmit')
+      console.log({ values })
 
-      mutate(values);
-    });
+      mutate(values)
+    })
 
     watchEffect(() => {
       if (isError.value && !isLoading.value) {
-        router.replace({ name: 'patients.index' });
+        router.replace({ name: 'patients.index' })
       }
-    });
+    })
 
     watch(
       patient,
       () => {
-        if (!patient) return;
+        if (!patient) return
 
         resetForm({
           values: patient.value,
-        });
+        })
       },
       {
         deep: true,
         immediate: true,
       },
-    );
+    )
 
     watch(isUpdateSuccess, (value) => {
-      if (!value) return;
+      if (!value) return
 
       // todo: toast(Cadena actualizada)
 
       resetForm({
         values: updatedPatient.value,
-      });
-    });
+      })
+    })
 
     return {
+      // modal,
+
       values,
       errors,
       meta,
@@ -135,6 +142,6 @@ export default defineComponent({
 
       // actions
       onSubmit,
-    };
+    }
   },
-});
+})
--- src/modules/patient/views/PatientCreate.vue
+++ src/modules/patient/views/PatientCreate.vue
@@ -8,93 +8,92 @@
     </div>
     <!-- BEGIN: Wizard Layout -->
 
-      <div class="mt-10 border-t px-5 pt-10 sm:px-20">
-        <!-- <div class="text-base font-medium">Profile Settings</div> -->
-        <form @submit="onSubmit">
-            <div class="mt-5 grid grid-cols-12 gap-4 gap-y-5">
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Nombre" :isRequired="true" />
-                <InputBase
-                  type="text"
-                  v-model="nombre"
-                  v-bind="nombreAttrs"
-                  :error="errors.nombre"
-                  name="nombre"
-                  placeholder="Ejemplo: Mauricio"
-                />
-              </div>
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Primer apellido" :isRequired="true" />
-                <InputBase
-                  type="text"
-                  v-model="primer_apellido"
-                  v-bind="primer_apellidoAttrs"
-                  :error="errors.primer_apellido"
-                  name="primer_apellido"
-                  placeholder="Ejemplo: Colin"
-                />
-              </div>
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Segundo apellido" :isRequired="false" />
-                <InputBase
-                  type="text"
-                  v-model="segundo_apellido"
-                  v-bind="segundo_apellidoAttrs"
-                  :error="errors.segundo_apellido"
-                  name="segundo_apellido"
-                  placeholder="Ejemplo: Yepez"
-                />
-              </div>
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Fecha de nacimiento" :isRequired="false" />
-                <InputBaseDate
-                  type="date"
-                  v-model="fecha_nacimiento"
-                  v-bind="fecha_nacimientoAttrs"
-                  :error="errors.fecha_nacimiento"
-                  name="fecha_nacimiento"
-                />
-              </div>
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Télefono" :isRequired="false" />
-                <InputBase
-                  type="tel"
-                  v-model="telefono"
-                  v-bind="telefonoAttrs"
-                  :error="errors.telefono"
-                  name="telefono"
-                  placeholder="Ejemplo: 44 31 86 37 55"
-                />
-              </div>
-              <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
-                <InputLabel label="Correo eléctronico" :isRequired="false" />
-                <InputBase
-                  type="email"
-                  v-model="correo_electronico"
-                  v-bind="correo_electronicoAttrs"
-                  :error="errors.correo_electronico"
-                  name="correo_electronico"
-                  placeholder="Ejemplo: 44 31 86 37 55"
-                />
-              </div>
-
-              <div class="flex flex-col gap-2.5 col-span-12">
-                <InputLabel label="Observaciones" :isRequired="false" />
-                <TextArea
-                  v-model="observaciones"
-                  name="observaciones"
-                  placeholder="Ejemplo: tipo de alergias"
-                  :error="observaciones"
-                />
-              </div>
-            </div>
-
-          <div class="col-span-12 mt-10 flex items-center justify-center sm:justify-end">
-            <ButtonPrimary text="Registrar" @click="onSubmit" />
+    <div :class="['mt-10 border-t pt-10', !isInsideModal ? 'px-5 sm:px-20' : '']">
+      <!-- <div class="text-base font-medium">Profile Settings</div> -->
+      <form @submit="onSubmit">
+        <div class="mt-5 grid grid-cols-12 gap-4 gap-y-5">
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Nombre" :isRequired="true" />
+            <InputBase
+              type="text"
+              v-model="nombre"
+              v-bind="nombreAttrs"
+              :error="errors.nombre"
+              name="nombre"
+              placeholder="Ejemplo: Mauricio"
+            />
           </div>
-        </form>
-      </div>
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Primer apellido" :isRequired="true" />
+            <InputBase
+              type="text"
+              v-model="primer_apellido"
+              v-bind="primer_apellidoAttrs"
+              :error="errors.primer_apellido"
+              name="primer_apellido"
+              placeholder="Ejemplo: Colin"
+            />
+          </div>
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Segundo apellido" :isRequired="false" />
+            <InputBase
+              type="text"
+              v-model="segundo_apellido"
+              v-bind="segundo_apellidoAttrs"
+              :error="errors.segundo_apellido"
+              name="segundo_apellido"
+              placeholder="Ejemplo: Yepez"
+            />
+          </div>
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Fecha de nacimiento" :isRequired="false" />
+            <InputBaseDate
+              type="date"
+              v-model="fecha_nacimiento"
+              v-bind="fecha_nacimientoAttrs"
+              :error="errors.fecha_nacimiento"
+              name="fecha_nacimiento"
+            />
+          </div>
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Télefono" :isRequired="false" />
+            <InputBase
+              type="tel"
+              v-model="telefono"
+              v-bind="telefonoAttrs"
+              :error="errors.telefono"
+              name="telefono"
+              placeholder="Ejemplo: 44 31 86 37 55"
+            />
+          </div>
+          <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
+            <InputLabel label="Correo eléctronico" :isRequired="false" />
+            <InputBase
+              type="email"
+              v-model="correo_electronico"
+              v-bind="correo_electronicoAttrs"
+              :error="errors.correo_electronico"
+              name="correo_electronico"
+              placeholder="Ejemplo: 44 31 86 37 55"
+            />
+          </div>
 
+          <div class="flex flex-col gap-2.5 col-span-12">
+            <InputLabel label="Observaciones" :isRequired="false" />
+            <TextArea
+              v-model="observaciones"
+              name="observaciones"
+              placeholder="Ejemplo: tipo de alergias"
+              :error="observaciones"
+            />
+          </div>
+        </div>
+
+        <div class="col-span-12 mt-10 flex items-center justify-center sm:justify-end">
+          <ButtonPrimary text="Registrar" @click="onSubmit" />
+        </div>
+      </form>
+    </div>
   </div>
 
   <!-- <div class="grid grid-cols-2 mt-2">

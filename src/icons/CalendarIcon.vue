80b1cc693716007447a00c4cb3034baba086ee14
jeszDev <jjcr.dev@gmail.com>
2025-09-05 15:21:28 -0600

    icon: iconos para mostrar la informacion del paciente

---
+++ src/icons/CalendarIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-calendar-icon lucide-calendar stroke-[1.5] [--color:currentColor] stroke-(--color) fill-(--color)/25 mr-2 size-4"
+  >
+    <path d="M8 2v4" />
+    <path d="M16 2v4" />
+    <rect width="18" height="18" x="3" y="4" rx="2" />
+    <path d="M3 10h18" />
+  </svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

fca9e992b3cbae120b511a41daa075d061fdc771
jeszDev <jjcr.dev@gmail.com>
2025-09-05 12:31:10 -0600

    icon: preview

---
+++ src/icons/PreviewIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    data-lucide="eye"
+    class="lucide lucide-eye size-4 stroke-[1.5] [--color:currentColor] stroke-(--color) fill-(--color)/25 mr-1 h-4 w-4"
+  >
+    <path
+      d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
+    ></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

99a245d97631d6196cab3ee391231654299f01ab
jess-github <jjcr.fie@gmail.com>
2025-09-13 21:34:54 -0600

    change: se adapta citas a componente view

--- src/modules/patient/views/PatientAppointmentsIndex.vue
+++ src/modules/patient/views/PatientAppointmentsIndex.vue
@@ -1,9 +1,14 @@
 <template>
-  <TimeLine />
+  <TemplateView title="Citas del paciente">
+    <template #main>
+      <TimeLine />
+    </template>
+  </TemplateView>
 </template>
 
 <script setup lang="ts">
-import TimeLine from '../../common/components/TimeLine.vue'
+import TemplateView from '@/modules/template/components/TemplateView.vue';
+import TimeLine from '@/modules/common/components/TimeLine.vue';
 </script>
 
 <style scoped></style>

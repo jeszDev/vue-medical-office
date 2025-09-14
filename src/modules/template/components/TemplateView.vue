329ee1f147b277ae090ad3ae036b541fce56adbd
jess-github <jjcr.fie@gmail.com>
2025-09-13 21:33:53 -0600

    feat: compomente template-view

---
+++ src/modules/template/components/TemplateView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative">
+    <div class="mt-8 flex items-center">
+      <slot name="header">
+        <h2 class="mr-auto text-lg font-medium">{{ title }}</h2>
+      </slot>
+    </div>
+
+    <div class="mt-10 border-t pt-10 px-5 sm:px-20">
+      <slot name="main"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style scoped></style>

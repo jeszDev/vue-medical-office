2c4291325d648022ce4d19f9b103442eb9733a99
jess-github <jjcr.fie@gmail.com>
2025-10-11 23:25:13 -0600

    boton para agendar cita

--- src/modules/patient/views/PatientAppointmentsIndex.vue
+++ src/modules/patient/views/PatientAppointmentsIndex.vue
@@ -1,12 +1,20 @@
 <template>
   <TemplateView title="Citas del paciente">
     <template #main>
-      <TimeLine :patientId />
+      <div class="flex justify-end mb-2">
+        <ButtonBase text="Agendar nueva cita" :icon="PreviewIcon" color="primary" />
+      </div>
+
+      <div>
+        <TimeLine :patientId />
+      </div>
     </template>
   </TemplateView>
 </template>
 
 <script setup lang="ts">
+import ButtonBase from '@/modules/common/components/ButtonBase.vue';
+import PreviewIcon from '@/icons/PreviewIcon.vue';
 import TemplateView from '@/modules/template/components/TemplateView.vue';
 import TimeLine from '@/modules/common/components/TimeLine.vue';
 

d026aa319da32a10d577efb602155a215c1c99d9
jeszDev <jjcr.dev@gmail.com>
2026-01-31 00:40:26 -0600

    configuracion de vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,27 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
-// import Toast from 'vue-toastification'
-// import 'vue-toastification/dist/index.css'
+import Toast/* , { PluginOptions } */ from "vue-toastification";
+import "vue-toastification/dist/index.css";
+const toastOptions = {
+  position: "top-center",
+  timeout: 2000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.45,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  closeButton: "button",
+  icon: false,
+  rtl: false,
+
+  transition: "Vue-Toastification__fade",
+  maxToasts: 20,
+  newestOnTop: true
+}
+
 
 import App from './App.vue'
 import router from './router'
@@ -17,6 +36,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
-// app.use(Toast)
+app.use(Toast, toastOptions);
 
 app.mount('#app')

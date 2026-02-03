0962f1e146b68b3aa530bf3706b1bab2a9a6324c
jeszDev <jjcr.dev@gmail.com>
2026-02-03 16:27:06 -0600

    fix: se corrige endpont para editar paciente

--- src/modules/patient/actions/create-update-petient.action.ts
+++ src/modules/patient/actions/create-update-petient.action.ts
@@ -1,40 +1,33 @@
-import { medicalOfficeApi } from '@/api/medicalOfficeApi'
-import type { Patient } from '../interfaces/patient.interface'
-import { useToast } from "vue-toastification";
+import { medicalOfficeApi } from '@/api/medicalOfficeApi';
+import type { Patient } from '../interfaces/patient.interface';
+import { useToast } from 'vue-toastification';
 
 export const createUpdatePatientAction = async (patient: Partial<Patient>) => {
-  const toast = useToast();
+  // const toast = useToast();
 
   try {
     if (patient.id && patient.id !== '') {
-      return await updatePatient(patient)
+      return await updatePatient(patient);
     }
 
-    const { data } = await medicalOfficeApi.post<Patient>(`/patients`, patient)
+    const { data } = await medicalOfficeApi.post<Patient>(`/patients`, patient);
 
-    toast.success('Paciente creado correctamente');
-
-    return data
+    return data;
   } catch (error) {}
-}
+};
 
 const updatePatient = async (patient: Partial<Patient>) => {
-  const patientId = patient.id
-  delete patient.id
-  // TODO: delete user_id
-  // delete chainCustody.user_id;
-  delete patient.created_at
-  delete patient.updated_at
+  const patientId = patient.id;
+
+  delete patient.id;
+  delete patient.nombre_completo;
+  delete patient.creado_el;
 
   try {
-    const { data } = await medicalOfficeApi.patch<Patient>(
-      `/chains/${patientId}`,
-      patient,
-    )
-
-    return data
+    const { data } = await medicalOfficeApi.patch<Patient>(`/patients/${patientId}`, patient);
+    return data;
   } catch (error) {
-    console.log(error)
-    throw new Error('Error updating patient')
+    console.log(error);
+    throw new Error('Error updating patient');
   }
-}
+};

d1746f2166db6dd40f246d70e7484f95819554dd
jeszDev <jjcr.dev@gmail.com>
2026-02-05 23:15:02 -0600

    change: propiedades de la interfaz cita

--- src/modules/appointment/interfaces/appointment.interface.ts
+++ src/modules/appointment/interfaces/appointment.interface.ts
@@ -1,12 +1,11 @@
-import type { Patient } from "@/modules/patient/interfaces/patient.interface";
+import type { Patient } from '@/modules/patient/interfaces/patient.interface';
 
 export interface Appointment {
   id: number;
-  fecha: string;        // "2025-09-25"
-  hora_inicio: string;  // "19:34:13"
-  hora_fin: string;     // "20:04:13"
+  fecha_hora_inicio: string;
+  fecha_hora_termino: string;
   motivo: string;
-  observaciones: string;
+  observaciones_cita: string;
   medico_id: number;
   cita_estatus_id: number;
   pacientes: Patient[];

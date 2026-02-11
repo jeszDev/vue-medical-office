f8aa0b3f21fcee271ab50f4bee742cd4aa6a1bbf
jeszDev <jjcr.dev@gmail.com>
2026-02-10 23:09:29 -0600

    develop: cita en misma fecha y hora

--- src/modules/appointment/actions/create-update-appointment.action.ts
+++ src/modules/appointment/actions/create-update-appointment.action.ts
@@ -1,7 +1,14 @@
 import { medicalOfficeApi } from '@/api/medicalOfficeApi';
 import type { Appointment } from '../interfaces/appointment.interface';
 import { useAuthStore } from '@/modules/auth/stores/auth.store';
+import type { AxiosError } from 'axios';
+import { useToast } from 'vue-toastification';
 
+interface ApiErrorResponse {
+  message: string;
+  code?: string;
+}
+const toast = useToast();
 const authStore = useAuthStore();
 
 export const createUpdateAppointmentAction = async (appointment: Partial<Appointment>) => {
@@ -17,8 +24,17 @@ export const createUpdateAppointmentAction = async (appointment: Partial<Appoint
       { ...appointment, medico_id: authStore.user!.id },
     );
 
+    console.log(data);
+
     return data;
-  } catch (error) {}
+  } catch (error) {
+    const err = error as AxiosError<ApiErrorResponse>;
+
+    if (err.response?.status === 409 && err.response.data.code === 'APPOINTMENT_OVERLAP') {
+      throw new Error(err.response.data.message);
+      toast.error('Ya existe una cita con esa fecha y horario');
+    }
+  }
 };
 
 const updateappointment = async (appointment: Partial<Appointment>) => {

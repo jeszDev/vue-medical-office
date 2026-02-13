45dc9333aabfc671c31a01e550c930af877145de
jeszDev <jjcr.dev@gmail.com>
2026-02-13 15:51:17 -0600

    feat: mostrar modal al clickear cita

---
+++ src/modules/appointment/views/AppointmentCalendar.vue
@@ -0,0 +1,45 @@
+<template>
+  <TemplateView v-if="!isLoading" title="Cita">
+    <template #main>
+      <div class="grid grid-cols-12 gap-x-10 gap-y-9">
+        <div class="col-span-12 sm:col-span-12 md:col-span-12">
+          <div class="text-xs opacity-70">FECHA Y HORA DE LA CITA</div>
+          <div class="mt-1.5 flex items-center">
+            <div class="text-base">13/02/2026 de las 09:00 a las 10:00 hrs.</div>
+          </div>
+        </div>
+        <div class="col-span-12 sm:col-span-12 md:col-span-12">
+          <div class="text-xs opacity-70">MEDICO</div>
+          <div class="mt-1.5 flex items-center">
+            <div class="text-base">{{ appointment.motivo }}</div>
+          </div>
+        </div>
+        <div class="col-span-12 sm:col-span-12 md:col-span-12">
+          <div class="text-xs opacity-70">MOTIVO DE LA CITA</div>
+          <div class="mt-1.5 flex items-center">
+            <div class="text-base">{{ appointment.motivo }}</div>
+          </div>
+        </div>
+        <div class="col-span-12 sm:col-span-12 md:col-span-12">
+          <div class="text-xs opacity-70">OBSERVACIONES</div>
+          <div class="mt-1.5 flex items-center">
+            <div class="text-base">{{ appointment.observaciones_cita || 'SIN OBSERVACIONES' }}</div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </TemplateView>
+</template>
+
+<script setup lang="ts">
+import TemplateView from '@/modules/template/components/TemplateView.vue';
+import { Appointment } from '../interfaces/appointment.interface';
+
+interface Props {
+  appointment: Appointment;
+}
+
+defineProps<Props>();
+</script>
+
+<style scoped></style>
--- src/modules/appointment/views/AppointmentIndex.vue
+++ src/modules/appointment/views/AppointmentIndex.vue
@@ -25,7 +25,12 @@
         :today-button="false"
         :views="['day', 'week']"
         :events="calendarEvents"
+        @event-click="onEventClick"
       />
+
+      <DialogBasic id="modal-appointment-calendar" size="3xl">
+        <AppointmentCalendar :appointment="selectedAppointment" />
+      </DialogBasic>
     </template>
   </TemplateView>
 </template>
@@ -38,12 +43,18 @@ import { medicalOfficeApi } from '@/api/medicalOfficeApi';
 import { Appointment } from '../interfaces/appointment.interface';
 
 import TemplateView from '@/modules/template/components/TemplateView.vue';
+import DialogBasic from '@/modules/common/components/DialogBasic.vue';
+import AppointmentCalendar from '@/modules/appointment/Views/AppointmentCalendar.vue';
 import { ref, onMounted, computed } from 'vue';
 import { useQuery } from '@tanstack/vue-query';
 import { getAppointmentsAction } from '../actions/get-appointments.action';
+import { CalendarEvent } from '../interfaces/calendar-event.interface';
 
 const fromDate = ref<string | null>('2026-02-01');
 const toDate = ref<string | null>('2026-02-28');
+
+const selectedAppointment = ref<null | Appointment>(null);
+const showModal = ref(false);
 
 const {
   data: events,
@@ -57,23 +68,22 @@ const {
       start: new Date(appointment.fecha_hora_inicio),
       end: new Date(appointment.fecha_hora_termino),
       title: `${appointment.motivo}` ?? 'Cita médica',
+      appointment,
     })),
 });
 
 const calendarEvents = computed(() => (events.value ?? []).map((e) => ({ ...e })));
 
-// const formatDate = (date: Date) => {
-//   return date.toISOString().split('T')[0];
-// };
+const onEventClick = ({ event }: CalendarEvent) => {
+  console.log(event.appointment);
 
-// const onViewChange = ({ startDate, endDate }: any) => {
-//   fromDate.value = formatDate(startDate);
-//   toDate.value = formatDate(endDate);
-// };
+  selectedAppointment.value = event.appointment;
+  // showModal.value = true;
 
-// onMounted(() => {
-//   loadAppointments();
-// });
+  window.tailwind.Modal.getOrCreateInstance(
+    document.getElementById('modal-appointment-calendar'),
+  ).show();
+};
 </script>
 
 <style scoped></style>

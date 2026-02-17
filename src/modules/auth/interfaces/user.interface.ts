c010047acaf22078bd4a15ecba48a985d1190536
jeszDev <jjcr.dev@gmail.com>
2026-02-17 10:13:39 -0600

    change: interfaz de usuario

--- src/modules/auth/interfaces/user.interface.ts
+++ src/modules/auth/interfaces/user.interface.ts
@@ -2,7 +2,7 @@ export interface User {
   id: number;
   name: string;
   email: string;
-  email_verified_at: null;
-  created_at: Date;
-  updated_at: Date;
+
+  roles: string[];
+  permissions: string[];
 }

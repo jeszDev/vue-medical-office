161c74129184f8fe75f7dc659e3cdc0a804d2b08
jeszDev <jjcr.dev@gmail.com>
2026-02-17 16:34:07 -0600

    feat: cargar modal en nueva cita

--- src/modules/patient/views/PatientAppointmentsIndex.vue
+++ src/modules/patient/views/PatientAppointmentsIndex.vue
@@ -1,13 +1,23 @@
 <template>
-  <TemplateView title="Citas del paciente">
+  <TemplateView :title="`CITAS DEL PACIENTE`">
     <template #main>
       <div class="flex justify-end mb-2">
-        <ButtonBase text="Agendar nueva cita" :icon="PreviewIcon" color="primary" />
+        <ButtonBase
+          text="Nueva cita"
+          :icon="PreviewIcon"
+          color="primary"
+          data-tw-toggle="modal"
+          data-tw-target="#modal-appointment-create-or-edit"
+        />
       </div>
 
       <div>
         <TimeLine :patientId />
       </div>
+
+      <DialogBasic id="modal-appointment-create-or-edit" size="3xl">
+        <AppointmentCreateUpdate :patientId :is-inside-modal="true" />
+      </DialogBasic>
     </template>
   </TemplateView>
 </template>
@@ -17,6 +27,8 @@ import ButtonBase from '@/modules/common/components/ButtonBase.vue';
 import PreviewIcon from '@/icons/PreviewIcon.vue';
 import TemplateView from '@/modules/template/components/TemplateView.vue';
 import TimeLine from '@/modules/common/components/TimeLine.vue';
+import DialogBasic from '@/modules/common/components/DialogBasic.vue';
+import AppointmentCreateUpdate from '@/modules/appointment/views/AppointmentCreateUpdate.vue';
 
 interface Props {
   patientId: string;

8fa564f0001f79a2dd856b46e43af863444332ae
jeszDev <jjcr.dev@gmail.com>
2026-03-05 15:46:48 -0600

    change: adaptar al template

--- src/modules/patient/views/PatientCreate.vue
+++ src/modules/patient/views/PatientCreate.vue
@@ -1,17 +1,10 @@
 <template>
-  <!-- <div
-    class="pt-8 pb-12 px-7 z-10 relative xl:mt-8 before:absolute before:inset-0 before:opacity-[.07] before:bg-foreground before:rounded-4xl after:absolute after:inset-0 after:bg-[color-mix(in_oklch,_var(--color-background),_var(--color-foreground)_2%)] after:rounded-4xl after:border after:border-foreground/[.15] dark:after:opacity-[.59]"
-  > -->
-  <div class="relative z-20">
-    <div class="mt-8 flex items-center">
-      <h2 class="mr-auto text-lg font-medium">
-        {{ action === 'create' ? 'Registro de nuevo paciente' : 'Editar paciente' }}
-      </h2>
-    </div>
-    <!-- BEGIN: Wizard Layout -->
+  <TemplateView :title="action === 'create' ? 'REGISTRAR PACIENTE' : 'EDITAR PACIENTE'">
+    <template #main>
+      <!-- <FullScreenLoader v-if="isLoading" /> -->
 
-    <div :class="['mt-10 border-t pt-10', !isInsideModal ? 'px-5 sm:px-20' : '']">
-      <!-- <div class="text-base font-medium">Profile Settings</div> -->
+      <!-- <template v-else> -->
+      <!-- <template> -->
       <form @submit="onSubmit">
         <div class="mt-5 grid grid-cols-12 gap-4 gap-y-5">
           <div class="flex flex-col gap-2.5 col-span-12 sm:col-span-4">
@@ -95,20 +88,9 @@
           <ButtonPrimary :text="action === 'create' ? 'Registrar' : 'Editar'" @click="onSubmit" />
         </div>
       </form>
-    </div>
-  </div>
-
-  <!-- <div class="grid grid-cols-2 mt-2">
-    <pre class="bg-blue-200 p-2">
-      {{ JSON.stringify(values, null, 2) }}
-    </pre>
-    <pre class="bg-red-200 p-2">
-      {{ JSON.stringify(errors, null, 2) }}
-    </pre>
-    <pre class="bg-green-200 p-2 col-span-2">
-      {{ JSON.stringify(meta, null, 2) }}
-    </pre>
-  </div> -->
+      <!-- </template> -->
+    </template>
+  </TemplateView>
 </template>
 
 <script src="./PatientCreate.ts" lang="ts"></script>
